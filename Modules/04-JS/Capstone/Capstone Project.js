74c747f436e8af9e9dfce33b9a64e3b117306cc6
Ting LC <106810213+LCT-ux@users.noreply.github.com>
2023-02-17 02:45:09 +0800

    Delete Capstone Project.js

--- Modules/04-JS/Capstone/Capstone Project.js
+++
@@ -1,6 +0,0 @@
-var date1 = new Date("06/30/2022");
-var date2 = new Date("07/30/2022");
-  
-var Difference_In_Time = date2.getTime() - date1.getTime();
-  
-var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24);
